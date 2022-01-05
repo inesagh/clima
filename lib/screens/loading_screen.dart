4ef79acb121f6a521441f12f3f22624f8d46a935
Inesa <inesaghalumyan@gmail.com>
2022-01-05 23:42:34 +0400

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocationData() async{
+
+    var weatherData = await WeatherModel().getLocationWeather();
+    sleep(Duration(seconds: 5));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => LocationScreen(weatherData)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 80,
+        ),
+      ),
+    );
+  }
+}
